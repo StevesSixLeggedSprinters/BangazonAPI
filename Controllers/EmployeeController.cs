6858e550176e6d6ad6b7a8204a0adf452831afd4
Kristin Caron <krissycaron@gmail.com>
2017-07-27 11:56:54 -0500

    Forgot to add the context of employee

--- Controllers/EmployeeController.cs
+++ Controllers/EmployeeController.cs
@@ -1,8 +1,12 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using BangazonAPI.Data;
+using BangazonAPI.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace BangazonAPI.Controllers
 {
@@ -23,13 +27,65 @@ namespace BangazonAPI.Controllers
         public IActionResult Get()
         {
             IQueryable<object> AllEmployees = from employee in _context.Employees select employee;
+
+            if (AllEmployees == null)
+            {
+
+                return NotFound();
+            }
+
+            return Ok(AllEmployees);
         }
 
+        //Get api/induvidualEmp/byID
+       [HttpGet("{id}", Name="GetCustomer")]
+       public IActionResult Get([FromRoute] int id)
+       {
+           if(!ModelState.IsValid)
+           {
+             BadRequest(ModelState);
+           }
+           try
+           {
+               Employee employee = _context.Employees.Single(e=>e.EmployeeId == id);
+               if (employee == null)
+               {
+                   return NotFound();
+               }
+               return Ok(employee);
 
+           }
+           catch(System.InvalidOperationException ex)
+           {
+               return NotFound();
+           }
+       }
+       [HttpPost]
+       public IActionResult Post([FromBody] Employee employee)
+       {
+           if(!ModelState.IsValid)
+           {
+             BadRequest(ModelState);
+           }
+           _context.Employee.Add(employee);
+
+           try
+           {
+            _context.SaveChanges();
+           }
+           catch(DbUpdateException)
+           {
+               if(EmployeeExists(employee.EmplpoyeeId))
+               {
+                   return new StatusCodeResult(StatusCodes.Status409Conflict);
+               }
+               else
+               {
+                   throw;
+               }
+           }
+           return CreatedAtRoute(GetEmployee, new {id = employee.EmplpoyeeId}, employee);
+       }
     }
-
-
-
-
 
 }
